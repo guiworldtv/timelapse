d4f59507aa4f6f9f7d1c94f3ebd60c6979f4cd2e
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-02-10 07:16:43 -0300

    Update cu.py

--- cu.py
+++ cu.py
@@ -4,44 +4,41 @@ import subprocess
 import os
 import time
 
-# URL do stream
-url = "https://5a7d54e35f9d2.streamlock.net/avt7/avt7.stream/chunklist_w127968702.m3u8"
+# URL do stream de vídeo
+video_url = "https://5a7d54e35f9d2.streamlock.net/morromendanha1/morromendanha1.stream/chunklist_w182959856.m3u8"
+
+# URL do stream de áudio
+audio_url = "http://200.137.217.155:8010/radiouniversitaria"
 
 # Tempo de captura de cada frame (em segundos)
-capture_time = 5
+capture_time = 2
 
 # Número de frames a serem capturados
-num_frames = 120
+num_frames = 240  # reduzido para 2 minutos de duração
 
-# Duração do vídeo final (em segundos)
-video_duration = 400
-
-# Taxa de quadros por segundo (FPS) da timelapse
+# FPS da timelapse
 fps = 30
 
-# Iniciar o stream
-streams = streamlink.streams(url)
-stream = streams["best"]
+# Iniciar o stream de vídeo
+video_streams = streamlink.streams(video_url)
+video_stream = video_streams["best"]
 
 # Iniciar a captura de frames
 for i in range(num_frames):
     filename = "frame_{}.png".format(i)
-    subprocess.call(["ffmpeg", "-i", stream.url, "-vframes", "1", filename])
+    subprocess.call(["ffmpeg", "-i", video_stream.url, "-vframes", "1", "-r", str(fps), filename])
     time.sleep(capture_time)
 
 # Criar o vídeo a partir dos frames capturados
 filenames = ["frame_{}.png".format(i) for i in range(num_frames)]
 clip = mp.ImageSequenceClip(filenames, fps=fps)
 
-# Ajustar a duração do vídeo
-clip = clip.set_duration(video_duration)
+# Adicionar a faixa de áudio ao vídeo
+audio_clip = mp.AudioFileClip(audio_url)
+final_clip = mp.CompositeVideoClip([clip, audio_clip])
 
-# Exportar o vídeo
-clip.write_videofile("timelapse.mp4", preset='ultrafast', fps=fps)
+final_clip.write_videofile("timelapse.mp4", preset='ultrafast')
 
 # Limpar os arquivos de frame
 for filename in filenames:
     os.remove(filename)
-
-# Baixar e salvar apenas o áudio do vídeo de exemplo
-subprocess.call(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "https://www.youtube.com/watch?v=RfYmwwQnPR8"])
